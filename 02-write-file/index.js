311df7de1067bc2f688ed1ce492275f4afb21179
Ilya Balykov <balykov.ilya.88@gmail.com>
2022-05-20 23:48:41 +0300

    feat: task 02 solved

---
+++ 02-write-file/index.js
@@ -0,0 +1,23 @@
+const { stdin, stdout } = process;
+const fs = require('fs');
+const path = require('path');
+
+process.on('exit', () => stdout.write('See you later\n'));
+process.on('SIGINT', () => {
+  process.exit();
+});
+fs.writeFile(
+  path.join(__dirname, 'text.txt'), '', (error) => {
+    if (error) throw error;
+    stdout.write('File created\n');
+  }
+);
+stdin.on('data', data => {
+  if (data.toString().trim() === 'exit') process.exit();
+  fs.appendFile(
+    path.join(__dirname, 'text.txt'), `${data}`, (error) => {
+      if (error) throw error;
+      stdout.write('Content added\n');
+    }
+  );
+});
